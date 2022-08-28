9545b4d872c85e1d8c8a08aa90ebd1b81e4f24d0
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-08-28 11:23:31 +0530

    Add files via upload

---
+++ 26/08.2022/circle.java
@@ -0,0 +1,15 @@
+package circle;
+
+public class circle {double radius;
+String color;
+circle(){
+ this.radius=1.0;
+this.color="red";}
+ circle(double r){
+	 radius=r;
+	 color="red";}
+	 double getRadius() {
+	 return radius;}
+	 double getArea() {
+		 return radius*radius*3.14;}}
+
