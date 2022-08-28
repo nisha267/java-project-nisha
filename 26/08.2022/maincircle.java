4dc79a2e2b223271f225148950d2c34ab000329e
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-08-28 11:24:54 +0530

    Add files via upload

---
+++ 26/08.2022/maincircle.java
@@ -0,0 +1,12 @@
+package circle;
+
+public class maincircle {public class mainclass {public static void main(String[]args)
+{circle crc=new circle();
+System.out.println("The radius of the circle is"+crc.getRadius()+"and the area is "+crc.getArea());
+circle crc2=new circle();
+System.out.println("The radius of the circle is"+crc2.getRadius()+"and the area is "+crc2.getArea());
+}
+}
+
+
+}
