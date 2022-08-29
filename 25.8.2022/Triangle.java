07e6686123e7c13c102ab1693df1dff911e477fa
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-08-29 09:02:43 +0530

    Update Triangle.java

--- 25.8.2022/Triangle.java
+++ 25.8.2022/Triangle.java
@@ -1,4 +1,4 @@
-
+/*Write a program to print the area and perimeter of a triangle having sides of 3, 4 and 5 units by creating a class named 'Triangle' without any parameter in its constructor.*/
 import java.util.*;
  public class Triangle {int p(int a,int b,int c)
 	 {int perimeter=a+b+c;
