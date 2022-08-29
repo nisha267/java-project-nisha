259ea2860fe26b372c66b77255dfd9df4ef7116d
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-08-29 08:58:27 +0530

    Add files via upload

---
+++ 25.8.2022/Mainnumber.java
@@ -0,0 +1,10 @@
+package evenmethod;
+
+public class Mainnumber {public static void main(String args[]) {
+	Number nb=new Number();
+	int n=24;
+	String res=nb.evenodd(n);
+	System.out.println(res);
+}
+
+}
