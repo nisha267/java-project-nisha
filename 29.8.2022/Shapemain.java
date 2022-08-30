c43280128d39bac639c96b44dc3aa014c965f473
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-08-30 13:40:46 +0530

    Update Shapemain.java

--- 29.8.2022/Shapemain.java
+++ 29.8.2022/Shapemain.java
@@ -18,14 +18,3 @@ public class Shapemain {
 }
 
 }
-//parent class//
- class Shape { int s;
-
-}
-//child class//
-class Square extends Shape{
-int area;
-void Area() 
-{  area=s*s;
-System.out.println("Area of the square is"+area);
-}}
