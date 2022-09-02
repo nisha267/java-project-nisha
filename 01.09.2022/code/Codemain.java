42d9d5ba2821ae85461bf190e6c98107ac047db0
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-09-02 09:20:54 +0530

    Update Codemain.java

--- 01.09.2022/code/Codemain.java
+++ 01.09.2022/code/Codemain.java
@@ -1,3 +1,5 @@
+/*Write a program to define a class Item containing code and price. Accept this data for five objects using array of objects. Display code, price in tabular form and also, display total price of all items.*/
+
 import java.util.Scanner;
 
 public class Codemain {public static void main(String args[])
