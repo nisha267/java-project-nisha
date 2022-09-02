94e53c4425f30ac4450927053d8b69a0eb8b741e
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-09-02 09:01:49 +0530

    Add files via upload

---
+++ 01.09.2022/Apple.java
@@ -0,0 +1,41 @@
+package apple.java;
+
+import java.util.Scanner;
+
+public class Apple {public static void main(String args[])
+{
+float app[]= new float[7];
+app[0]=(float) 2.3;
+app[1]=(float) 7.3;
+app[2]=(float) 8.3;
+app[3]=(float) 6.3;
+app[4]=(float) 3.3;
+app[5]=(float) 9.3;
+app[6]=(float) 10.3;
+
+float avg=0;
+Scanner sc=new Scanner(System.in);
+try
+{
+	for(int i=0;i<7;i++)
+	{
+	System.out.println(app[i]);
+	}
+
+	for(int i=0;i<7;i++)
+	{
+		avg+=app[i];
+	}
+	
+	avg=avg/7;
+	System.out.println("Average sales of week : "+avg);
+}
+
+catch(Exception e)
+{
+	System.out.println("I/O Error");
+}
+}
+}
+
+
