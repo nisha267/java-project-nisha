dc36122f6032fb59c33f14142028b68904fe9d15
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-09-02 12:43:06 +0530

    Update Students.java

--- 2.09.2022/Students.java
+++ 2.09.2022/Students.java
@@ -1,3 +1,4 @@
+/* Write a program to print the roll number and average marks of 8 students in three subjects (each out of 100). The marks are entered by user.*/
 package javaarray;
 
 import java.util.Scanner;
