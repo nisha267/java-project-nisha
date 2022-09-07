8eb482421169a021ef06998432ac5c04592c4e37
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-09-07 12:59:51 +0530

    Add files via upload

---
+++ 07.09.2022/Junit/Largearr.java
@@ -0,0 +1,12 @@
+package com.java.unit.junit_testing;
+
+public class Largearr {int max=0;
+	int maximum(int arr[]) {
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]>max) 
+		{max=arr[i];
+	}}
+return max;
+}}
+
+
---
+++ 07.09.2022/Junit/Unitexample.java
@@ -0,0 +1,21 @@
+package com.java.unit.junit_testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+class Unitexample {
+
+	@Test
+	void test() {
+	{
+Largearr ob=new Largearr();
+int arr[]= {60,389,56,10};
+int actual=ob.maximum(arr);
+int expected=389;
+assertEquals(expected,actual);
+
+}}}
+
+
+
