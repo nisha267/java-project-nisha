878203fd3a6f884fe07703752275daf2f8e516ff
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-09-12 11:19:22 +0530

    Add files via upload

---
+++ 12.09.2022/Reversestring.java
@@ -0,0 +1,11 @@
+package string;
+
+public class Reversestring {
+	public static void main(String[]args) {
+		String s="Riya";
+		for(int i=s.length()-1;i>=0;i--) {
+			System.out.println(s.charAt(i));
+		}
+	}
+
+}
