4a73281fce23bb3d1eadb1faf19bf2e731d6603c
nisha267 <111556178+nisha267@users.noreply.github.com>
2022-09-13 09:03:10 +0530

    Update MismatchException.java

--- 12.09.2022/DeleteConsonant/MismatchException.java
+++ 12.09.2022/DeleteConsonant/MismatchException.java
@@ -1,4 +1,4 @@
-
+//3. Write a program to throw a user-defined exception "String Mismatch Exception", if two strings are not equal (ignore the case).
 package string;
 
 public class MismatchException extends Exception {
